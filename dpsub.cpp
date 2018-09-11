17e5e561617be9fa4549fd8a74fa2e960997d0d2
Yiding Cui <winoros@gmail.com>
2018-09-11 20:08:37 +0800

    disjoint set is not used

--- dpsub.cpp
+++ dpsub.cpp
@@ -4,30 +4,6 @@
 using std::vector;
 using std::pair;
 
-struct disjointSet {
-    vector<int> p;
-    disjointSet(int n);
-    int findRoot(int x);
-    void join(int x, int y);
-};
-
-disjointSet::disjointSet(int n) {
-    p.resize(n);
-    for (int i = 0; i < n; ++ i) {
-        p[i] = i;
-    }
-}
-
-int disjointSet::findRoot(int x) {
-    return p[x] == x ? x : p[x] = findRoot(p[x]);
-}
-
-void disjointSet::join(int x, int y) {
-    int px = findRoot(x);
-    int py = findRoot(y);
-    p[px] = py;
-}
-
 int totCnt;
 
 void simulateDP(int s1, int s2, vector<int> &dp, vector<pair<int, int>> &edges) {
